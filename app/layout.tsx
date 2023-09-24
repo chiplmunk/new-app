512e742d3edd71efc85169a708056a408d045159
Anav Gupta <anavguptadelhi@gmail.com>
2023-09-24 20:06:29 +0530

    add assets and layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,20 +1,48 @@
-import { ClerkProvider } from '@clerk/nextjs'
-import React from 'react'
- 
+import { ClerkProvider } from "@clerk/nextjs";
+import React from "react";
+import { Inter, Space_Grotesk } from "next/font/google";
+import type { Metadata } from "next";
+import "./globals.css";
+
+const inter = Inter({
+  subsets: ["latin"],
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+  variable: "--font-inter",
+});
+
+const spaceGrotesk = Space_Grotesk({
+  subsets: ["latin"],
+  weight: ["300", "400", "500", "600", "700"],
+  variable: "--font-spaceGrotesk",
+});
+
 export const metadata = {
-  title: 'Next.js 13 with Clerk',
-}
- 
+  title: "Dev Flow",
+  description: "A community-driven forum for answering all types of questions.",
+  icons: {
+    icon: "/assets/images/site-logo.svg",
+  },
+};
+
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
-    <ClerkProvider>
-      <html lang="en">
-        <body>{children}</body>
-      </html>
+    <ClerkProvider
+      appearance={{
+        elements: {
+          formButtonPrimary: "primary-gradient",
+          footerActionLink: "primary-text-gradient hover:text-primary-500",
+        },
+      }}
+    >
+      <div>
+        <div className={`${inter.variable} ${spaceGrotesk.variable}`}>
+          {children}
+        </div>
+      </div>
     </ClerkProvider>
-  )
+  );
 }
